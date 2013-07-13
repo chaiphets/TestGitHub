6ebd4eb506ebc8daffbdc43154249d8962f9e427
Rabbiters <chaiphetsa@gmail.com>
2013-07-13 14:48:33 +0700

    Change database config

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'freelancer';
-$db['default']['password'] = 'hifour';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'admin';
 $db['default']['database'] = 'rabbiters_framework';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
